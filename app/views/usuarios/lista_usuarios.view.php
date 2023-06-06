447f99e3e4a9b944acb83efb4ad8f9cee45925d3
zam <zam23pc@gmail.com>
2023-06-06 01:22:34 -0600

    tabla usuario agregada

---
+++ app/views/usuarios/lista_usuarios.view.php
@@ -0,0 +1,18 @@
+<h1>Lista Usuarios</h1>
+<div class="table-responsive">
+<table class="table">
+  <thead>
+    <tr class="table-dark">
+      <th scope="col">#</th>
+      <th scope="col">Usuario</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Editar</th>
+      <th scope="col">Eliminar</th>
+    </tr>
+  </thead>
+  <tbody id="lista-usuarios"></tbody>
+</table>
+</div>
+
+<a class="btn btn-primary" href="#">📝</a>
